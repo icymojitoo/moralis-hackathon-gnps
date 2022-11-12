be3d28d0414694e47eaa3bd6bb7ff34478f96e34
icymojitoo <mohitshi@icloud.com>
2022-11-12 06:11:17 +0530

    new

--- pages/my-assets.js
+++ pages/my-assets.js
@@ -71,7 +71,7 @@ export default function MyAssets() {
                         <p>{nft.description}</p>
                         <div className='card-actions justify-end'>
                             <p className='text-xl font-bold text-primary'>
-                                {nft.price} MATIC
+                                {nft.price} MATICs
                             </p>
                         </div>
                     </div>
